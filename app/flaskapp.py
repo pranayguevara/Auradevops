58e3154ae23b93efb47c835105901356c4b502b7
pranay <pranaybablu024@gmail.com>
2025-10-04 21:11:00 +0530

    First push of flaskapp

---
+++ app/flaskapp.py
@@ -0,0 +1,11 @@
+# app/app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🚀 Deployed via Jenkins CI/CD on AWS ECS!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
